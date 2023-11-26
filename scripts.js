7fd2e33bddedf0956674fc4d8eb36aef475cc286
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-11-26 17:02:37 +0000

    Add mobile header, menu and fixes
    Add a mobile header nav bar - search icon, site logo and animated hamburger menu
    Add mobile menu to appear on hamburger click
    Add mobile header and mobile menu transition animations in and out
    Add mobile menu icons, link ul, socials and contact details
    Chnge mobile menu styling and positioning
    Change hero section h1, h5 font size and spacing for desktop and mobile screens
    Change hero background image size for mobile screens

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 console.log("stylesheet connected");
+
+/* Mobile menu open and animation function */
+document.addEventListener('DOMContentLoaded', function() {
+    /* Select full hamburger menu */
+    const hamMenu = document.querySelector('.menu-icon');
+    /* Select the span elemets from HamMenu */
+    const spanLines = hamMenu.querySelectorAll('span');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const mobileHeader = document.querySelector('.mobile-nav');
+
+    /* Add click lister to toggle change-ham-color class for span 0 and 2 */
+    hamMenu.addEventListener('click', function() {
+        /* Hamburger span toggles */
+        spanLines[0].classList.toggle('change-ham-color');
+        spanLines[2].classList.toggle('change-ham-color');
+        /* Mobile header background colour change */
+        mobileHeader.classList.toggle('mobile-nav-background');
+
+        /* Menu pop in toogle class */
+        mobileMenu.classList.toggle('show-mobile-menu');
+    });
+});
+
